39a6b6da076b88fa5934c313145db3028f677625
Srirahul M <srirahul.m@amphisoft.in>
2023-03-19 11:41:14 +0530

    feat: bubble animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import './App.css';
-import { TypeAnimation } from 'react-type-animation';
 
 function App() {
   return (
-    <div className="App">
-      <div class="text">
-        Hello, I'm <span class="highlight">Srirahul M</span>.
+   <>
+    <div class="wrapper">
+    <h1>
+    
+        Hello, I'm Srirahul M
         <br/>
-        <TypeAnimation
-          sequence={[
-            `I'm a frontend developer.`,
-            3000,
-            `I'm a full-stack web developer.`,
-          ]}
-          repeat={false}
-          cursor={false}
-        />
-      </div>
-    </div>
+        I'm a full-stack web developer.
+    </h1>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+  </div>
+   </>
   );
 }
 
